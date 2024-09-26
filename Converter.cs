d13728ab4abce536226e0607b69b6e0d87d2f6e2
Felicia-Lindblad <lindblad.felicia@gmail.com>
2024-09-26 20:19:39 +0200

    Add project files.

---
+++ Converter.cs
@@ -0,0 +1,10 @@
+﻿namespace _2lönProject
+{
+    public class Converter
+    {
+        public double BeräknaLönEfterSkatt(double lönInnanSkatt, double skatt)
+        {
+            return lönInnanSkatt - (lönInnanSkatt * (skatt / 100));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2lönProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej och välkommen till lönmätaren");
+            Console.WriteLine("Skriv in din lön här;");
+
+            string lönString = Console.ReadLine()!;
+            double lön = Convert.ToDouble(lönString);
+
+            Console.WriteLine($"Din lön innan skatt är {lön}");
+            Console.WriteLine("Skriv in din skatt i procent");
+
+            string skattString = Console.ReadLine()!;
+            double skatt = Convert.ToDouble(skattString);
+
+            Converter converter = new Converter();
+
+            double lönEfterSkatt = converter.BeräknaLönEfterSkatt(lön, skatt);
+
+            Console.WriteLine($"Din lön efter skatt är : {lönEfterSkatt}");
+        }
+    }
+}
